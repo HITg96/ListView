28defbb7b2bfd2121d798a000d02c9a8f92ef42c
Jezus-San <michalt948@gmail.com>
2023-02-02 09:35:38 +0100

    lista Dynamiczna

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -3,6 +3,15 @@ package com.example.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.ListaListViewKategoria);
+        AdapterView.OnItemClickListener clickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String kategoria = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(MainActivity.this, kategoria, Toast.LENGTH_LONG).show();
+            }
+        };
+        listView.setOnItemClickListener(clickListener);
+
+
+
+
+
+        //DYNAMICZNA
+        ArrayList<String> slowa = new ArrayList<>();
+        slowa.add("Pogłaszcz Kota");
+        slowa.add("Zamiaucz ");
+        slowa.add("Wyjście z psem");
+        slowa.add("Nauczyć się potrójnej całki");
+        ArrayAdapter<String> slowaAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, slowa); //simple_list_item pozwala wyświetlić String
+        //Adapter ołączony z Danymi z tablicy slowa
+
+        ListView listview2 = findViewById(R.id.ListViewDynamicznie);
+        listview2.setAdapter(slowaAdapter);
+
+
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        EditText editText = findViewById(R.id.editTextPersonalnie);
+                        String wpisanyText = editText.getText().toString();
+                        slowa.add(wpisanyText);
+                        slowaAdapter.notifyDataSetChanged();
+                    }
+                }
+        );
+
+        AdapterView.OnItemClickListener kliknieteDynamiczne = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, slowa.get(i), Toast.LENGTH_SHORT).show();
+                slowa.remove(i);
+                slowaAdapter.notifyDataSetChanged();
+            }
+        };
+        listview2.setOnItemClickListener(kliknieteDynamiczne);
+
+
     }
 }
